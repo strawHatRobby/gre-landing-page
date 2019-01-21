fe7c37ea10a7c556036e2b57ac008095de4e015a
robs100DaysOfCode <robfr93@icloud.com>
2019-01-21 14:58:59 +0530

    Updates

---
+++ precache-manifest.9651c0afb816be8a95ce257f8af165ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gre-landing-page/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0283455ad0f51911820b",
+    "url": "/gre-landing-page/static/js/runtime~main.0283455a.js"
+  },
+  {
+    "revision": "7775d67719c4b304b9a7",
+    "url": "/gre-landing-page/static/js/main.7775d677.chunk.js"
+  },
+  {
+    "revision": "701e80665e0aa5267d43",
+    "url": "/gre-landing-page/static/js/1.701e8066.chunk.js"
+  },
+  {
+    "revision": "7775d67719c4b304b9a7",
+    "url": "/gre-landing-page/static/css/main.ec331a7a.chunk.css"
+  },
+  {
+    "revision": "df01b9e01f446afef0d39b5ede73821b",
+    "url": "/gre-landing-page/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/gre-landing-page/precache-manifest.fd99302a59a31845fd0c6ab45a2cf601.js"
+  "/gre-landing-page/precache-manifest.9651c0afb816be8a95ce257f8af165ac.js"
 );
 
 workbox.clientsClaim();
