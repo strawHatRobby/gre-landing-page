0c39811c39e09f38542257d0b86eef5d292b3760
robs100DaysOfCode <robfr93@icloud.com>
2019-01-21 14:07:35 +0530

    Updates

---
+++ precache-manifest.05bc9342892bfd1205de24de8b47db91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "980567fa891fbcff62f1",
+    "url": "/static/js/main.980567fa.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "980567fa891fbcff62f1",
+    "url": "/static/css/main.89105702.chunk.css"
+  },
+  {
+    "revision": "f1026b57be58e484a49841c3b6d712f1",
+    "url": "/index.html"
+  }
+];
